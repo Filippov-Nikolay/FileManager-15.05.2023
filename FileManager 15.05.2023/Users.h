76fbf8096f57446254f060562814dbe583be361e
Filippov-Nikolay <filippovnikolay195@gmail.com>
2023-10-15 12:39:23 +0300

    Реализовал заголовочный файл Users

--- FileManager 15.05.2023/Users.h
+++ FileManager 15.05.2023/Users.h
@@ -1,6 +1,30 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
 
 class Users {
+protected:
+	// LogUs* logus;
+	string firstName; // Имя
+	string lastName; // Фамилия
+	string login; // Логин
+	string password; // Пароль
 
+	bool firstEntry; // Первый вход
+	bool access; // Доступ
+public:
+	void Input();
+	void Print() const;
+
+
+	// Аксессоры
+	// Геттеры
+	string GetFirstName() const;
+	string GetLastName() const;
+	string GetLogin() const;
+	string GetPassword() const;
+	
+	bool GetFirstEntry() const;
+	bool GetAccess() const;
 };
-
